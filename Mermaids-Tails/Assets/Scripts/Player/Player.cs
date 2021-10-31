37ef5f0df412cf7f8467d3ffd3319cfc42cf12f6
Thijs <thijs@uni-bremen.de>
2021-10-31 16:22:33 +0200

    ADD: New Input System

--- Mermaids-Tails/Assets/Scripts/Player/Player.cs
+++ Mermaids-Tails/Assets/Scripts/Player/Player.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+
     // variables for gameplay
     public GameObject playerCamera;
 
@@ -32,8 +34,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetInput();
-
         // if player is moving, the moving animation will displayed
         if (direction.x != 0 || direction.y != 0)
         {
@@ -43,19 +43,7 @@ public class Player : MonoBehaviour
         {
             playerAnimator.SetLayerWeight(1, 0);
         }
-
         MovePlayer();
-
-        // 0 left click, 1 right click, 2 middle click
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject currentProjectile = Instantiate(projectileType, transform.position, Quaternion.identity);
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 currentPosition = transform.position;
-            Vector2 projectileDirection = (mousePosition - currentPosition).normalized;
-            currentProjectile.GetComponent<Rigidbody2D>().velocity = projectileDirection * projectileForce;
-            currentProjectile.GetComponent<Projectile>().damage = Random.Range(minimumDamage, maximumDamage);
-        }
     }
 
     // Updated ist called fixed by time
@@ -64,6 +52,33 @@ public class Player : MonoBehaviour
         // make a 3D Vector of Player Position and Z-Camera Position
         Vector3 currentCameraPosition = new Vector3(transform.position.x, transform.position.y, playerCamera.transform.position.z);
         playerCamera.transform.position = currentCameraPosition;
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        direction = value.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            GameObject currentProjectile = Instantiate(projectileType, transform.position, Quaternion.identity);
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            Vector2 currentPosition = transform.position;
+            Vector2 projectileDirection = (mousePosition - currentPosition).normalized;
+            currentProjectile.GetComponent<Rigidbody2D>().velocity = projectileDirection * projectileForce;
+            currentProjectile.GetComponent<Projectile>().damage = Random.Range(minimumDamage, maximumDamage);
+            // Do attack animation
+        }
+    }
+
+    public void OnDash(InputAction.CallbackContext value)
+    {
+        if (value.started)
+        {
+            transform.Translate(direction * speed * dash * Time.deltaTime);
+        }
     }
 
     // set the player weapon ans weapon stats
@@ -79,53 +94,6 @@ public class Player : MonoBehaviour
     {
         transform.Translate(direction * speed * Time.deltaTime);
         ChangeAnimationDirection(direction);
-    }
-
-    // get player input
-    private void GetInput()
-    {
-        direction = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            direction += Vector2.up;
-            currentDirection = Facing.Up;
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            direction += Vector2.down;
-            currentDirection = Facing.Down;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            direction += Vector2.left;
-            currentDirection = Facing.Left;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            direction += Vector2.right;
-            currentDirection = Facing.Right;
-        }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            Vector2 targetPosition = Vector2.zero;
-            if (currentDirection == Facing.Up)
-            {
-                targetPosition.y = 1;
-            } 
-            else if (currentDirection == Facing.Down)
-            {
-                targetPosition.y = -1;
-            }
-            else if (currentDirection == Facing.Left)
-            {
-                targetPosition.x = -1;
-            }
-            else if (currentDirection == Facing.Right)
-            {
-                targetPosition.x = 1;
-            }
-            transform.Translate(targetPosition * dash);
-        }
     }
 
     // Change the direction of animation in the animation class
